017460eee3580d1b5a917762446c32b7e58a0eb6
Lakshmi Sahithi V <83357155+sahu189@users.noreply.github.com>
2022-09-09 23:41:15 +0530

    Create error.js

---
+++ views/error.js
@@ -0,0 +1,12 @@
+<% layout('layouts/boilerplate')%>
+
+
+<div class="card col-3 offset-3 p-3" style="width: 38rem;">
+  <img src="https://res.cloudinary.com/dfgv4kake/image/upload/v1662539687/YelpCamp/err.jpg" class="card-img-top" alt="404!">
+  <div class="card-body">
+    <div class="alert alert-danger" role="alert">
+    <p style="text-align: center;"> 404!!<br><br>Page is not found.</p> 
+    </div>
+    <a href="/campgrounds" class="btn btn-outline-primary btn-block">Back to campgrounds!</a>
+  </div>
+</div>
